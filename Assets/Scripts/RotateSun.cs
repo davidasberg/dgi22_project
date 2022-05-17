451cd80c49bfe2098a1f5067476739fc8e51628a
dasberg <dasberg@kth.se>
2022-05-17 17:04:59 +0200

    Added sun rotation

---
+++ Assets/Scripts/RotateSun.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSun : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //rotate y axis
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, Time.time * 20, transform.rotation.eulerAngles.z);
+    }
+}
